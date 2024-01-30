800b85b5a683a2a9ea0539bbadd507f3df720b76
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2024-01-30 16:16:37 +0200

    init project

---
+++ front-end/src/App.tsx
@@ -0,0 +1,12 @@
+import viteLogo from '/vite.svg';
+
+const App = () => {
+    return (
+        <>
+            <h1 className="text-2xl font-bold">Hello World!</h1>
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+        </>
+    );
+};
+
+export default App;
