e0a7eab43f7f610a741b800bfba24a8494e16ffa
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2024-01-30 16:25:38 +0200

    init dev branch

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,7 +3,7 @@ import viteLogo from '/vite.svg';
 const App = () => {
     return (
         <>
-            <h1 className="text-2xl font-bold">Hello World!</h1>
+            <h1 className="text-2xl font-bold">Hello World!!</h1>
             <img src={viteLogo} className="logo" alt="Vite logo" />
         </>
     );
