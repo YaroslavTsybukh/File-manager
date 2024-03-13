1587245b6c2406188fdb398247d77903c44268d2
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2024-03-13 11:24:47 +0200

    table is ready

--- front-end/src/components/homePage/Content.tsx
+++ front-end/src/components/homePage/Content.tsx
@@ -1,4 +1,4 @@
-import { Plus, Info } from 'lucide-react';
+import { Info, Download, Trash2 } from 'lucide-react';
 import { FC } from 'react';
 
 export const Content: FC = () => {
@@ -26,6 +26,47 @@ export const Content: FC = () => {
                             <option value="2">Upload File</option>
                         </select>
                         <Info />
+                    </div>
+                </div>
+                <div className="table">
+                    <div className="table__head">
+                        <ul className="table__row">
+                            <li className="table__header table__header_name">
+                                <p>Name</p>
+                            </li>
+                            <li className="table__header table__header_date">
+                                <p>Date</p>
+                            </li>
+                            <li className="table__header table__header_size">
+                                <p>Size</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="table__body">
+                        <ul className="table__row">
+                            <li className="table__data table__data_image">
+                                <img
+                                    src="/rose.jpg"
+                                    width={25}
+                                    alt="content image"
+                                />
+                            </li>
+                            <li className="table__data table__data_name">
+                                <p>test</p>
+                            </li>
+                            <li className="table__data table__data_download">
+                                <Download />
+                            </li>
+                            <li className="table__data table__data_trash">
+                                <Trash2 />
+                            </li>
+                            <li className="table__data table__data_date">
+                                <p>12.03.2024</p>
+                            </li>
+                            <li className="table__data table__data_size">
+                                <p>10mb</p>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
